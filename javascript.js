621fe08bfc65ee099bc9a7a9f120b7ffdbacc387
Pratham Joshi <pratham.cmd@gmail.com>
2026-02-26 23:01:14 +0530

    hamburger menu

--- javascript.js
+++ javascript.js
@@ -77,3 +77,12 @@ buttons.forEach(btn => {
   });
 
 });
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+if(hamburger){
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+}
